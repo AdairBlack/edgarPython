3d7d76d95159ad2a6b10b78756b4b247a8eb88d0
edgar <2805449211@qq.com>
2023-05-04 00:21:57 +0800

    try new things

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 message = "What the fuck"
 
+firstName = "Edgar"
+secondName = "Adair"
+
+fullName = f"{firstName}.{secondName}"
+
+myList = ['computer', 'math', 'deeplearning', 'python']
+
+
+# python test
 print(message)
+print(fullName)
+print(myList)
+
+
+for item in myList:
+    print(item)
+
+# generate a function to add 2 numbers
+
